a4507b1d67111724e49c2c4b84d233fe4f9187bd
amirrezaask <raskarpour@gmail.com>
2020-04-18 13:34:08 -0400

    init

---
+++ rhythmbox.el
@@ -0,0 +1,6 @@
+;;; rhythmbox.el --- Rhythmbox controller via dbus   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  AmirrezaAskarpour
+
+;; Author: AmirrezaAskarpour <raskarpour@gmail.com>
+;; Keywords: abbrev, abbrev, 
